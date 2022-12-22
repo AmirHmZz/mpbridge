96843d63f81f5678cc18254c016f1aade235b07a
Amirreza Hamzavi <38752106+AmirHmZz@users.noreply.github.com>
2022-12-22 19:56:52 +0330

    Add print_progress_bar() to utils

--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,20 @@ def open_dir(filename):
     else:
         opener = "open" if sys.platform == "darwin" else "xdg-open"
         subprocess.call([opener, filename])
+
+
+def print_progress_bar(
+        iteration,
+        total,
+        prefix='',
+        suffix='',
+        decimals=1,
+        length=100,
+        fill='█',
+        print_end="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=print_end)
+    if iteration == total:
+        print()
